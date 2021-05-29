7ed28b19fe6a1901e1f9f3e847c89632e06ce571
Alexandru Tapirdea <tapirdea.alexandru@yahoo.com>
2021-05-30 01:05:44 +0300

    collapsable menu

--- src/components/Separator.js
+++ src/components/Separator.js
@@ -1,8 +1,12 @@
 import * as React from "react";
 import "./Separator.scss";
 
-const Separator = () => {
-  return <div className="separator__container"></div>;
+const Separator = ({ section }) => {
+  return (
+    <div className="separator__container" id={section}>
+      <h2 className="separator__title">{section}</h2>
+    </div>
+  );
 };
 
 export default Separator;
